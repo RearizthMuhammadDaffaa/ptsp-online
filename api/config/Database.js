15d762e1375702faa804c81ec58c632775a2d43e
RearizthMD <astarohhellprince@gmail.com>
2024-10-15 14:34:30 +0700

    add video api

--- api/config/Database.js
+++ api/config/Database.js
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
-
+import mysql2 from "mysql2"
 const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_USER,{
   host:process.env.DB_HOST,
   dialect:'mysql',
-  dialectModule:require('mysql2')
+  dialectModule:mysql2
 });
 
 export default db;
