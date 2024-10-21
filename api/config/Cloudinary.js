7e02d7398dbfa651607bff9d0dde341b97e21a28
RearizthMD <astarohhellprince@gmail.com>
2024-10-21 17:33:19 +0700

    add cloudinary as alternative

--- api/config/Cloudinary.js
+++ api/config/Cloudinary.js
@@ -5,10 +5,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Cloudinary configuration
+// cloudinary.v2.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "dtxawyrz2",
+  api_key: "971812266884377",
+  api_secret: "3lNrfiK4OcRqzEuvfJlywI8XDqc",
 });
 
 export default cloudinary.v2;
