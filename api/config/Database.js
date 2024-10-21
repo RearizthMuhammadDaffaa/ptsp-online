a8183336adeca845c5ebb58eaffddd13fc9ce471
RearizthMD <astarohhellprince@gmail.com>
2024-10-21 16:59:38 +0700

    add cloudinary as alternative

--- api/config/Database.js
+++ api/config/Database.js
@@ -2,12 +2,14 @@ import { Sequelize } from "sequelize";
 import mysql2 from "mysql2"
 import dotenv from 'dotenv';
 dotenv.config();
-const db = new Sequelize(
-  process.env.DB_NAME,process.env.DB_USER,process.env.DB_PW,{
-  host:process.env.DB_HOST,
-  dialect:'mysql',
-  dialectModule:mysql2
-});
+// const db = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PW,{
+//   host:process.env.DB_HOST,
+//   dialect:'mysql',
+//   dialectModule:mysql2
+// });
 // const db = new Sequelize(
 //   "sql12738419",
 //   "sql12738419",
@@ -17,15 +19,15 @@ const db = new Sequelize(
 //   dialect:'mysql',
 //   dialectModule:mysql2
 // });
-// const db = new Sequelize(
-//   "rianapri_ptsp_pa_sumedang",
-//   "rianapri_daffaa_77",
-//  "daffaa123456daffaa;;",
-//   {
-//   host:"api-ptsp.rianaprilyawan.web.id",
-//   dialect:'mysql',
-//   dialectModule:mysql2
-// });
+const db = new Sequelize(
+  "rianapri_ptsp_pa_sumedang",
+  "rianapri_daffaa_77",
+ "daffaa123456daffaa;;",
+  {
+  host:"api-ptsp.rianaprilyawan.web.id",
+  dialect:'mysql',
+  dialectModule:mysql2
+});
 // const db = new Sequelize(
 //   "pstp-pa-sumedang",
 //   "root",
