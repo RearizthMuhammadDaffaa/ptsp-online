d35abf29835f913da7c6de5d784872f604ea39c9
Rearizth <astarothhellprince@gmail.com>
2024-11-26 13:09:10 +0700

    fix image

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import logo from "../assets/PA-SUMEDANG.png";
+import logo from "../assets/navbarIMG/PA-SUMEDANG.png";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import useAuth from "../hooks/useAuth";
